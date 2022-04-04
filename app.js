82ca917d7ff234fcf740dba09be5141788c65b63
Kenneth Quach <kennethtquach@gmail.com>
2022-04-04 00:29:31 -0700

    connect mongoDB

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
+const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const bodyParser = require('body-parser');
 
-app.get("/", (req, res) => res.send("Hello World!!"));
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.use("/api/users", users);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
