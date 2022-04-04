cf8a92f9ecc4edc9ac71125c67a137379ff79097
Justin Nguyen <judnguyen@ucdavis.edu>
2022-04-04 10:04:59 -0700

    Add config/keys

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const db = require('./config/keys').mongoURI;
 const users = require("./routes/api/users");
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -14,8 +14,8 @@ app.get("/", (req, res) => res.send("Hello World!"));
 
 app.use("/api/users", users);
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
