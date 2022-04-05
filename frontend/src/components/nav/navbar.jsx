a9de69c07b2feac597e1c205a142fca8687be8e8
Justin Nguyen <judnguyen@ucdavis.edu>
2022-04-05 15:03:53 -0700

    Style navbar

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './navbar.css'
 
 class NavBar extends React.Component {
   constructor(props) {
@@ -13,15 +14,20 @@ class NavBar extends React.Component {
 
   showLinks() {
     if (this.props.loggedIn) {
-      return <div>
-        <Link exact to="/">Home</Link>
-        <Link exact to="/account">My Account</Link>
-        <button onClick={(e) => this.logoutUser(e)}>LOGOUT</button>
+      return <div className='navbar-item'>
+        <Link exact to="/" className='logo'>TheftDeflect</Link>
+        <ul className='nav_links'>
+          <li><button className='navbar-button'><Link exact to="/account" className='my-account'>MY ACCOUNT</Link></button></li>
+          <li><button className='navbar-button' onClick={(e) => this.logoutUser(e)} >LOGOUT</button></li>
+        </ul>
       </div>
     } else {
-      return <div>
-        <button onClick={() => this.props.openModal('login')}>LOG IN</button>
-        <button onClick={() => this.props.openModal('signup')}>SIGN UP</button>
+      return <div className='navbar-item'>
+        <Link exact to="/" className='logo'>TheftDeflect</Link>
+        <ul className='nav_links'>
+          <li><button className='navbar-button' onClick={() => this.props.openModal('login')}>LOG IN</button></li>
+          <li><button className='navbar-button' onClick={() => this.props.openModal('signup')}>SIGN UP</button></li>
+        </ul>
       </div>
     }
   }
