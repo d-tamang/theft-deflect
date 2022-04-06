9d8b89784e0703ea50a98797191bf420eb6324a0
Justin Nguyen <judnguyen@ucdavis.edu>
2022-04-05 22:41:52 -0700

    Add new script to package.json

--- app.js
+++ app.js
@@ -10,13 +10,6 @@ const pins = require('./routes/api/pins');
 const comments = require('./routes/api/comments');
 const path = require('path');
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
-}
-
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
@@ -38,4 +31,11 @@ app.use("/api/comments", comments);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
