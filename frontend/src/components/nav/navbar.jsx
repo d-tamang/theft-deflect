d55ecded553b743dcb5a336473b19eca1398280b
Disnee Tamang <d.tamang48@yahoo.com>
2022-04-07 16:58:56 -0700

    css

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -19,7 +19,7 @@ class NavBar extends React.Component {
           <div><button className='nav-btn'><Link className="nav-link" to="/">MAP</Link></button></div>
           <div><button className='nav-btn'><Link className="nav-link" to="/account">ACCOUNT</Link></button></div>
         </div>
-        <div id="nav-logo">Theft Deflect</div>
+        <div id="nav-logo"><Link className="nav-link" to="/">Theft Deflect</Link></div>
         <div className="right-nav">
           <div><button className='nav-btn'><Link className="nav-link" to="/about">ABOUT</Link></button></div>
           <div><button className='nav-btn' onClick={(e) => this.logoutUser(e)} >LOG OUT</button></div>
@@ -31,7 +31,7 @@ class NavBar extends React.Component {
           <div><button className='nav-btn'><Link className="nav-link" to="/">MAP</Link></button></div>
           <div><button className='nav-btn'><Link className="nav-link" to="/about">ABOUT</Link></button></div>
         </div>
-        <div id="nav-logo">Theft Deflect</div>
+        <div id="nav-logo"><Link className="nav-link" to="/">Theft Deflect</Link></div>
         <div className="right-nav">
           <div><button className='nav-btn' onClick={() => this.props.openModal('login')}>LOG IN</button></div>
           <div><button className='nav-btn' onClick={() => this.props.openModal('signup')}>SIGN UP</button></div>
