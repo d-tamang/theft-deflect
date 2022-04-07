75d660958ac04fe8e2a5f8a0c3d83c4a00c3fca5
Disnee Tamang <d.tamang48@yahoo.com>
2022-04-06 17:07:06 -0700

    about page

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -6,6 +6,7 @@ import NavBarContainer from './nav/navbar_container';
 import MapWrapper from './map/map_wrapper';
 import AccountContainer from './nav/account_container';
 import PinShowContainer from './pin/pin_show_container';
+import AboutPage from './nav/about_page';
 import './app.css';
 
 const App = () => (
@@ -14,6 +15,7 @@ const App = () => (
     <NavBarContainer />
     <Switch>
       <ProtectedRoute exact path="/account" component={AccountContainer} />
+      <Route exact path="/about" render={AboutPage}/>
       <Route exact path="/pins/:pinId" component={PinShowContainer} />
       <Route exact path="/" component={MapWrapper} />
       <Redirect to="/" />
---
+++ frontend/src/components/nav/about_page.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <div>Meet the Developers!</div>
+      
+    </div>
+  )
+}
+
+export default AboutPage;
